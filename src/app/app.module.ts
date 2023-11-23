d5aafb9e0683bc1b2fc09d85d534fdf971150af7
Ivan <82239564+HAlim1999@users.noreply.github.com>
2023-11-22 23:20:49 -0300

    AngularBasic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { CounterModule } from './counter/components/counter.module';
+import { HeroModule } from './heroes/hero.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CounterModule,
+    HeroModule
   ],
   providers: [],
   bootstrap: [AppComponent]
